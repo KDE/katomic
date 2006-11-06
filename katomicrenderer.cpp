c8c5d68ce0edf720ea83681f87c95fb7a948122b
Mauricio Piacentini <piacentini@kde.org>
2006-11-06 13:54:53 +0000

    Correct typos in license headers
    svn path=/trunk/KDE/kdegames/katomic/; revision=602662

--- katomicrenderer.cpp
+++ katomicrenderer.cpp
@@ -4,12 +4,12 @@
  *
  * This file is part of the KDE project "KAtomic"
  *
- * KReversi is free software; you can redistribute it and/or modify
+ * KAtomic is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
  *
- * KReversi is distributed in the hope that it will be useful,
+ * KAtomic is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
