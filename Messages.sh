4bd8e87e5e95953a8ad0cc139a8f52137dca0c76
Laurent Montel <montel@kde.org>
2007-05-14 11:38:24 +0000

    Revert last changes
    svn path=/trunk/KDE/kdegames/katomic/; revision=664584

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.rc *.ui *.kcfg > rc.cpp
 : > levelnames.cpp
 for i in levels/level_*; do
 egrep '^Name=' $i | sed -e 's#^Name=\(.*\)$#i18n(\"\1\")#' >> levelnames.cpp
