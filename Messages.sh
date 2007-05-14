da91ca269bf7dd1f70365fa38fbcf823f3f4e71f
Laurent Montel <montel@kde.org>
2007-05-14 10:08:41 +0000

    translate rc file ui file
    svn path=/trunk/KDE/kdegames/katomic/; revision=664553

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,5 @@
-#! /bin/sh
+#! /usr/bin/env bash
+$EXTRACTRC *.rc *.ui *.kcfg > rc.cpp
 : > levelnames.cpp
 for i in levels/level_*; do
 egrep '^Name=' $i | sed -e 's#^Name=\(.*\)$#i18n(\"\1\")#' >> levelnames.cpp
