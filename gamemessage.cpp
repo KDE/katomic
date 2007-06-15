7ca01b63fdb2bd48bcd80e54a6986da2529c0802
Dmitry Suzdalev <dimsuz@gmail.com>
2007-06-15 18:32:07 +0000

    Add copyright headers
    svn path=/trunk/KDE/kdegames/katomic/; revision=676024

--- gamemessage.cpp
+++ gamemessage.cpp
@@ -1,3 +1,25 @@
+/*******************************************************************
+ *
+ * Copyright 2007 Dmitry Suzdalev <dimsuz@gmail.com>
+ *
+ * This file is part of the KDE project "KAtomic"
+ *
+ * KAtomic is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * KAtomic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KAtomic; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ ********************************************************************/
 #include "gamemessage.h"
 #include <QPainter>
 #include <QTimeLine>
