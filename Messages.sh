5673970444f61aa36a945e7739e1638048f40aec
Burkhard Lück <lueck@hube-lueck.de>
2009-10-28 12:15:04 +0000

    remove message extraction to katomic catalog, this is now done via desktop files (*levels.dat)
    svn path=/trunk/KDE/kdegames/katomic/; revision=1041732

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,2 @@
 #! /usr/bin/env bash
-: > levelnames.cpp
-for i in levels/level_*; do
-egrep '^Name=' $i | sed -e 's#^Name=\(.*\)$#i18n(\"\1\")#' >> levelnames.cpp
-done
 $XGETTEXT *.cpp -o $podir/katomic.pot
-rm -f levelnames.cpp
