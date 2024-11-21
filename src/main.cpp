d0a3202be10aac7c124c4916a1fef44e9f1a209b
Laurent Montel <montel@kde.org>
2024-11-21 13:25:01 +0100

    Fix theme/icon on windows

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,15 @@
 #include <KCrash>
 #include <KMessageBox>
 #include <KDBusService>
+#define HAVE_KICONTHEME __has_include(<KIconTheme>)
+#if HAVE_KICONTHEME
+#include <KIconTheme>
+#endif
 
+#define HAVE_STYLE_MANAGER __has_include(<KStyleManager>)
+#if HAVE_STYLE_MANAGER
+#include <KStyleManager>
+#endif
 #include <QTimer>
 #include <QCommandLineParser>
 #include <QCommandLineOption>
@@ -26,8 +34,17 @@
 
 int main(int argc, char **argv)
 {
+#if HAVE_KICONTHEME
+    KIconTheme::initTheme();
+#endif
     QApplication app(argc, argv);
-
+#if HAVE_STYLE_MANAGER
+    KStyleManager::initStyle();
+#else // !HAVE_STYLE_MANAGER
+#if defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+    QApplication::setStyle(QStringLiteral("breeze"));
+#endif // defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+#endif // HAVE_STYLE_MANAGER
     KLocalizedString::setApplicationDomain(QByteArrayLiteral("katomic"));
 
     KAboutData aboutData( QStringLiteral("katomic"), i18n("KAtomic"),
